cc6790256c361052683afc8e375db161bb429089
Max Strübing <mxstrbng@gmail.com>
2017-03-22 06:52:40 +0100

    test(fileUtils) write tests for filterFiles

--- src/utils/file-utils.test.js
+++ src/utils/file-utils.test.js
@@ -1,4 +1,4 @@
-import {editorconfigPath, fileExists} from './file-utils';
+import {fileExists, filterFiles, editorconfigPath} from './file-utils';
 
 test('editorconfig exist in project-root', () => {
 	expect(fileExists(editorconfigPath())).toBeTruthy();
@@ -14,4 +14,12 @@ test('editorconfigPath ends with .editorconfig', () => {
 	const editorconfigPathEnd = editorconfigPathString.substring(editorconfigPathString.length - editorconfig.length);
 
 	expect(editorconfigPathEnd).toEqual('.editorconfig');
+});
+
+test('should return true when not matching file', () => {
+	expect(filterFiles('/some/path/index.js', 'notIndex.js$')).toBeTruthy();
+});
+
+test('should return false when matching file', () => {
+	expect(filterFiles('/some/path/index.js', 'index.js$')).toBeFalsy();
 });
