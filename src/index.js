a83519c681c86ae5a3849b5b77e22f3fe4cf7e7d
Max Strübing <mxstrbng@gmail.com>
2017-04-04 13:31:19 +0200

    feat(output): add pretty finish output

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import {fileExists, fileNotEmpty, filterFiles, editorconfigPath} from './utils/f
 import {log, info, error} from './logger/logger';
 
 // No editorconfig no fun
-/* eslint-disable no-unused-expressions */
-!fileExists(editorconfigPath()) && (error(`ERROR: no .editorconfig found: ${editorconfigPath()}`) || process.exit(1));
-/* eslint-enable */
+if (!fileExists(editorconfigPath())) {
+	error(`ERROR: no .editorconfig found: ${editorconfigPath()}`);
+	process.exit(1);
+}
 
 const editorconfig = iniparser.parseSync(editorconfigPath());
+let checkedFiles = 0;
+const errors = 1;
 
 const filterOptions = {
 	regex: '.git|node_modules|coverage|dist',
@@ -27,6 +30,7 @@ finder.on('match', (strPath, stat) => {
 	log(strPath);
 	info('just test output');
 	error(stat.size);
+	checkedFiles++;
 });
 
 finder.on('patherror', (err, strPath) => {
@@ -35,6 +39,20 @@ finder.on('patherror', (err, strPath) => {
 
 finder.on('error', err => {
 	error(`Global Error ${err}`);
+});
+
+finder.on('complete', () => {
+	log('all done');
+	if (errors === 0) {
+		log(`sucessfully checked ${checkedFiles} files`);
+	} else {
+		error(`${errors} occured! See log above and fix errors`);
+		if (errors < 254) {
+			process.exit(errors);
+		} else {
+			process.exit(254);
+		}
+	}
 });
 
 log(editorconfig);
