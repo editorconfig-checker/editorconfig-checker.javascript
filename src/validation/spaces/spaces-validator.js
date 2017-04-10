5f6d0cbf5fee082e63652216f939dbadcd1b297d
Max Strübing <mxstrbng@gmail.com>
2017-04-10 20:12:32 +0200

    feat(validation): add some tests and better validation, still needed block comments

--- src/validation/spaces/spaces-validator.js
+++ src/validation/spaces/spaces-validator.js
@@ -3,16 +3,16 @@ import {error} from '../../logger/logger';
 const validate = (line, lineNumber, file, editorconfig) => {
 	if (editorconfig.indent_style === 'space') {
 		if (editorconfig.indent_size) {
-			if (line.match(/^( *)\w/)) {
+			if (line.match(/^( *)[\w\/]/)) {
 				// TODO: Handling block comments(*)
-				if (!(line.match(/^( *)\w/)[1].length % editorconfig.indent_size === 0)) {
-					error(`Not the right amount of leftpadding spaces in ${file} on line ${lineNumber}`);
+				if (line.match(/^( *)[\w\/]/)[1].length % editorconfig.indent_size !== 0) {
+					error(`Not the right amount of left-padding spaces in ${file} on line ${lineNumber}`);
 					return false;
 				}
 			}
 		}
 
-		if (!line.match(/^ *\w/)) {
+		if (!line.match(/^ *[\w\/]/)) {
 			error(`Mixed indentation in ${file} on line ${lineNumber}`);
 			return false;
 		}
--- src/validation/spaces/spaces-validator.test.js
+++ src/validation/spaces/spaces-validator.test.js
@@ -24,8 +24,32 @@ test('should return true if text starts directly no matter of indent_style', () 
 	expect(validate(line,lineNumber, file, editorconfig)).toBeTruthy();
 });
 
-test('should return true if text has the right amount of leftpadding space', () => {
+test('should return true if comment starts directly no matter of indent_style', () => {
+	const line = '/Hello';
+	const lineNumber = 1;
+	const file = 'sample.txt';
+	const editorconfig = {
+		indent_style: 'tab',
+		indent_size: 4
+	};
+
+	expect(validate(line,lineNumber, file, editorconfig)).toBeTruthy();
+});
+
+test('should return true if comment has the right amount of leftpadding space', () => {
 	const line = '  Hello';
+	const lineNumber = 1;
+	const file = 'sample.txt';
+	const editorconfig = {
+		indent_style: 'space',
+		indent_size: 2
+	};
+
+	expect(validate(line,lineNumber, file, editorconfig)).toBeTruthy();
+});
+
+test('should return true if comment has the right amount of leftpadding space', () => {
+	const line = '  /Hello';
 	const lineNumber = 1;
 	const file = 'sample.txt';
 	const editorconfig = {
@@ -39,6 +63,20 @@ test('should return true if text has the right amount of leftpadding space', () 
 test('should return false if text has not the right amount of leftpadding space', () => {
 	console.error = jest.fn();
 	const line = '   Hello';
+	const lineNumber = 1;
+	const file = 'sample.txt';
+	const editorconfig = {
+		indent_style: 'space',
+		indent_size: 4
+	};
+
+	expect(validate(line,lineNumber, file, editorconfig)).toBeFalsy();
+	expect(console.error).toBeCalled();
+});
+
+test('should return false if comment has not the right amount of leftpadding space', () => {
+	console.error = jest.fn();
+	const line = '   /Hello';
 	const lineNumber = 1;
 	const file = 'sample.txt';
 	const editorconfig = {
@@ -64,9 +102,37 @@ test('should return false if text has left padding tabs', () => {
 	expect(console.error).toBeCalled();
 });
 
+test('should return false if comment has left padding tabs', () => {
+	console.error = jest.fn();
+	const line = ' \t/Hello';
+	const lineNumber = 1;
+	const file = 'sample.txt';
+	const editorconfig = {
+		indent_style: 'space',
+		indent_size: 4
+	};
+
+	expect(validate(line,lineNumber, file, editorconfig)).toBeFalsy();
+	expect(console.error).toBeCalled();
+});
+
 test('should return false if text has left padding tabs', () => {
 	console.error = jest.fn();
 	const line = '\tHello';
+	const lineNumber = 1;
+	const file = 'sample.txt';
+	const editorconfig = {
+		indent_style: 'space',
+		indent_size: 4
+	};
+
+	expect(validate(line,lineNumber, file, editorconfig)).toBeFalsy();
+	expect(console.error).toBeCalled();
+});
+
+test('should return false if text has left padding tabs', () => {
+	console.error = jest.fn();
+	const line = '\t/Hello';
 	const lineNumber = 1;
 	const file = 'sample.txt';
 	const editorconfig = {
