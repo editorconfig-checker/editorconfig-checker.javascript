e496c6e456981413ba413923d24a6e1418620de3
Max Strübing <mxstrbng@gmail.com>
2017-04-19 10:22:09 +0200

    feat(validation): handle block comments for spaces

--- src/validation/spaces/spaces-validator.js
+++ src/validation/spaces/spaces-validator.js
@@ -4,7 +4,6 @@ const validate = (line, lineNumber, file, editorconfig) => {
 	if (line.length > 0 && editorconfig.indent_style === 'space') {
 		if (editorconfig.indent_size) {
 			if (line.match(/^( *)[\w\/]/)) {
-				// TODO: Handling block comments(*)
 				const indentSize = line.match(/^( *)[\w\/\*]/)[1].length;
 				if (indentSize % editorconfig.indent_size !== 0 && line[indentSize] !== '*') {
 					error(`Not the right amount of left-padding spaces in ${file} on line ${lineNumber}`);
