94438d5a6a2b284e32def7aa4cdf7f64ef78285b
Max Strübing <mxstrbng@gmail.com>
2017-08-13 18:05:16 +0200

    refactor(spacesValidator): use one if clause instead of two

--- src/validation/space/space-validator.js
+++ src/validation/space/space-validator.js
@@ -1,14 +1,12 @@
 import {error} from '../../logger/logger';
 
 const validate = (line, lineNumber, file, editorconfig) => {
-	if (line.length > 0 && editorconfig.indent_style === 'space') {
-		if (editorconfig.indent_size) {
-			if (line.match(/^( *)[\w\/]/)) {
-				const indentSize = line.match(/^( *)[\w\/\*]/)[1].length;
-				if (indentSize % editorconfig.indent_size !== 0 && line[indentSize] !== '*') {
-					error(`Not the right amount of left-padding spaces in ${file} on line ${lineNumber}`);
-					return false;
-				}
+	if (line.length > 0 && editorconfig.indent_style === 'space' && editorconfig.indent_size) {
+		if (line.match(/^( *)[\w\/]/)) {
+			const indentSize = line.match(/^( *)[\w\/\*]/)[1].length;
+			if (indentSize % editorconfig.indent_size !== 0 && line[indentSize] !== '*') {
+				error(`Not the right amount of left-padding spaces in ${file} on line ${lineNumber}`);
+				return false;
 			}
 		}
 
