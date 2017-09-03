eb44a65d18c47a92735beee1ba87a0b1358f8130
Max Strübing <mxstrbng@gmail.com>
2017-09-03 11:21:10 +0200

    feat(editorconfig): deep merging of editorconfig

--- src/editorconfig/editorconfig.test.js
+++ src/editorconfig/editorconfig.test.js
@@ -7,7 +7,31 @@ test(`should get basic rules`, () => {
 	const editorconfig = getEditorconfig('someFile.js');
 
 	const expected = {
+		indent_style: 'space', // eslint-disable-line camelcase
 		indent_size: '4' // eslint-disable-line camelcase
+	};
+
+	expect(editorconfig).toEqual(expected);
+});
+
+test(`should get basic rules`, () => {
+	const editorconfig = getEditorconfig('someFile.php');
+
+	const expected = {
+		indent_style: 'space', // eslint-disable-line camelcase
+		indent_size: '2' // eslint-disable-line camelcase
+	};
+
+	expect(editorconfig).toEqual(expected);
+});
+
+test(`should compose rules`, () => {
+	const editorconfig = getEditorconfig('1/someFile.js');
+
+	const expected = {
+		indent_style: 'space', // eslint-disable-line camelcase
+		indent_size: '4', // eslint-disable-line camelcase
+		trim_trailing_whitespace: 'true' // eslint-disable-line camelcase
 	};
 
 	expect(editorconfig).toEqual(expected);
