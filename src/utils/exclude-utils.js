275e509af073684140d021dc12bf21fae7498ac2
Max Strübing <mxstrbng@gmail.com>
2017-12-18 15:34:44 +0100

    fix(filter): specify end of the string explicitly

--- src/utils/exclude-utils.js
+++ src/utils/exclude-utils.js
@@ -29,28 +29,28 @@ const getDefaultExcludes = () => {
 // https://github.com/editorconfig-checker/editorconfig-checker.javascript/issues/18
 const getDefaultExcludesRegexp = () => {
 	return [
-		'vendor',
-		'node_modules',
+		'vendor/',
+		'node_modules/',
 		'coverage/',
 		'.DS_Store/',
 		'.git/',
-		'.gif',
-		'.png',
-		'.bmp',
-		'.jpg',
-		'.svg',
-		'.ico',
-		'.lock',
-		'.eot',
-		'.woff',
-		'.woff2',
-		'.ttf',
-		'.bak',
-		'.bin',
-		'.min.js',
-		'.min.css',
-		'.pdf',
-		'.jpeg'
+		'.gif$',
+		'.png$',
+		'.bmp$',
+		'.jpg$',
+		'.svg$',
+		'.ico$',
+		'.lock$',
+		'.eot$',
+		'.woff$',
+		'.woff2$',
+		'.ttf$',
+		'.bak$',
+		'.bin$',
+		'.min.js$',
+		'.min.css$',
+		'.pdf$',
+		'.jpeg$'
 	];
 };
 
