6a24a6e7aed2389d66b465f63b00122257c791bc
Max Strübing <mxstrbng@gmail.com>
2019-05-02 08:22:05 +0200

    chore(release): 2.1.0

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import {
     removeFile
 } from "./utils";
 
-const CORE_VERSION = "1.1.3";
+const CORE_VERSION = "1.2.0";
 
 const execute = () => {
     const ecProcess = spawn(`${binary()}`, process.argv.slice(2));
