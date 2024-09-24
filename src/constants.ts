c78ecb0c123a71888e1165d17bfc22daf40ceb0d
Théo LUDWIG <contact@theoludwig.fr>
2024-09-24 10:19:40 +0200

    fix!: correct working directory
    BREAKING CHANGE: Minimum supported Node.js >= 20.11.0
    
    Ref: https://nodejs.org/api/esm.html#importmetadirname

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ import path from "node:path"
 
 export const NAME = "editorconfig-checker"
 export const VERSION = process.env.EC_VERSION ?? "latest"
-export const CWD = process.cwd()
+export const CWD = path.join(import.meta.dirname, "..")
 export const BIN_PATH = path.join(CWD, "bin")
 export const COMBINED_PATH = path.join(BIN_PATH, VERSION)
